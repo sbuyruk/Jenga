318ec23e84201611fcce4001b79f7d943287130e
sbuyruk@gmail.com <sbuyruk@gmail.com>
2022-11-01 08:08:12 +0300

    Sidebar veritabanından gelen verileri iç içe menü şeklinde gösterir hale geldi. Henüz linkleri yok.

--- Jenga.Web/wwwroot/js/menutree.js
+++ Jenga.Web/wwwroot/js/menutree.js
@@ -1,75 +1,35 @@
 ﻿
 $(document).ready(function () {
-    loadSliderMenu();
-});
-var data =  [
-    {
-        text: "Node 1",
-        icon: "fa fa-folder",
-        expanded: true,
-        nodes: [
-            {
-                text: "Sub Node 1",
-                icon: "fa fa-folder",
-                nodes: [
-                    {
-                        id: "sub-node-1",
-                        text: "Sub Child Node 1",
-                        icon: "fa fa-folder",
-                        class: "dropdown-menu",
-                        href: "https://google.com"
-                    },
-                    {
-                        text: "Sub Child Node 2",
-                        icon: "fa fa-folder"
-                    }
-                ]
-            },
-            {
-                text: "Sub Node 2",
-                icon: "fa fa-folder"
-            }
-        ]
-    },
-    {
-        text: "Node 2",
-        icon: "fa fa-folder"
-    },
-    {
-        text: "Node 3",
-        icon: "fa fa-folder"
-    },
-    {
-        text: "Node 4",
-        icon: "fa fa-folder"
-    },
-    {
-        text: "Node 5",
-        icon: "fa fa-folder"
+    let url = '/Admin/MenuTanim/GetMenuAll';
+
+    try {
+
+        $.getJSON(url, function (data) {
+            buildMenu($('#menu'), data);
+            $('#menu').menu();
+        });
+        function buildMenu(parent, items) {
+            $.each(items, function () {
+                var li = $('<li>' + this.text + '</li>');
+                if (!this.isActive) {
+                    li.addClass("ui-state-disabled");
+                }
+                li.appendTo(parent);
+                if (this.nodes && this.nodes.length > 0) {
+                    var ul = $('<ul></ul>');
+                    ul.appendTo(li);
+                    buildMenu(ul, this.nodes);
+                }
+            });
+        }  
+    } catch (e) {
+        console.log('error');
     }
-];
 
-function loadSliderMenu() {
-    //$.getJSON('Admin/MenuTanim/GetMenuAll', function (data) {
-
-    //    var text = `text: ${data.text}<br>
-    //                id: ${data.time}<br>`
-
-
-    //    $(".mySidebar").html(text);
-    //});
-    $(jQuery.parseJSON(JSON.stringify(data))).each(function () {
-
-        var text = `id: ${this.id}<br>
-                    text: ${this.text}<br>`
-
-
-        $(".mySidebar").html(text);
-    });
-};
-var isClosed = true;
-function menuTreeFunction(x) {
-    x.classList.toggle("change");
+});
+//Open - Close Sidebar with animation
+function openCloseSideBar(menuIconsDiv) {
+    menuIconsDiv.classList.toggle("change");
     if (isClosed) {
         openNav();
         isClosed = false;
@@ -79,9 +39,10 @@ function menuTreeFunction(x) {
     }
     
 }
+var isClosed = true;
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
 }
 
 function closeNav() {
