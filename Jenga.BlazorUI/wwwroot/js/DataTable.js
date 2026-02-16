520162feabb50a44bbda747ee629eed25cc0686a
sbuyruk@gmail.com <sbuyruk@gmail.com>
2026-02-16 09:34:16 +0300

    DataTable kaldırılıyor

--- Jenga.BlazorUI/wwwroot/js/DataTable.js
+++ Jenga.BlazorUI/wwwroot/js/DataTable.js
@@ -7,6 +7,7 @@
         },
     });
 };
+
 window.initDataTable = (data) => {
     new DataTable('#DataTable', {
         layout: {
@@ -16,56 +17,27 @@ window.initDataTable = (data) => {
         },
     });
 };
+
 // Material Tables
-function initMaterialTableWithExport() {
-    $('#MaterialListTable').DataTable({
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-}
-function initMaterialCategoryTableWithExport() {
-    $('#MaterialCategoryListTable').DataTable({
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-}
-function initMaterialModelTableWithExport() {
-    $('#MaterialModelListTable').DataTable({
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-}
-function initMaterialBrandTableWithExport() {
-    $('#MaterialBrandListTable').DataTable({
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-}
+
 function initLocationTableWithExport() {
     $('#LocationListTable').DataTable({
         dom: 'Bfrtip',
         buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
     });
 }
-function initMaterialEntryTableWithExport() {
-    $('#MaterialEntryListTable').DataTable({
-        dom: 'Bfrtip',
-        buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-}
+
 // Custom Tables
 window.initCustomDataTable = (data) => {
     new DataTable('#CustomDataTable', {
         destroy: true,
         pageLength: 5,
         lengthChange: false,
-        dom: 'flrtip' // 'l' (length), 'r' (processing), 't' (table), 'i' (info), 'p' (pagination), 'f' (filter/search)
-        // Eğer arama kutusu görünmüyorsa 'f' ekleyin:
-        // dom: 'frtip'
+        dom: 'flrtip'
     });
 };
+
 window.initCustomDataTable1 = () => {
-    // Eğer tablo daha önce başlatıldıysa yok et
     if ($.fn.DataTable.isDataTable('#CustomDataTable1')) {
         $('#CustomDataTable1').DataTable().destroy();
     }
